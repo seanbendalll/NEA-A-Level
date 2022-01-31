c059c2197957b35d40417cc836e84abd0201afd8
Sean Bendall <seanbendall@Seans-MacBook-Pro.local>
2022-01-31 22:18:27 +0000

    Made changes to application file, made the model.py a class structure

--- application.py
+++ application.py
@@ -1,12 +1,26 @@
 import tkinter as tk
-from tkinter.ttk import *
+from tkinter import ttk
+from tkinter import font
+from tkinter import *
+from model import Model
 
 root = tk.Tk()
 root.title("ALCA - A Level Computer Science Application")
+root.resizable(False, False)
 root_size_x = 865
 root_size_y = 700
 root.geometry("{}x{}".format(root_size_x, root_size_y))
 
+data_model = Model()
+
+def showAnswer():
+    question_frame.grid_forget()
+    answer_frame.grid(column = 0, row = 6, columnspan = 4)
+
+def showQuestion():
+    answer_frame.grid_forget()
+    question_frame.grid(column = 0, row = 6, columnspan = 4)
+    
 def change_to_home_frame():
     home_frame.pack(fill = "both")
     test_frame.forget()
@@ -18,6 +32,7 @@ def change_to_test_frame():
     home_frame.forget()
     learn_frame.forget()
     progress_frame.forget()
+    showQuestion()
 
 def change_to_learn_frame():
     learn_frame.pack(fill = "both")
@@ -31,6 +46,9 @@ def change_to_progress_frame():
     learn_frame.forget()
     test_frame.forget()
 
+title_font = tk.font.Font(family = "Helvetica", size = 36, weight = "bold")
+topic_font = tk.font.Font(family = "Helvetica", size = 28)
+
 #using frames to give a multiple view application
 home_frame = tk.Frame(root, height = 500, width = 5)
 test_frame = tk.Frame(root, height = 500, width = 5)
@@ -38,7 +56,7 @@ learn_frame = tk.Frame(root, height = 500, width = 5)
 progress_frame = tk.Frame(root, height = 500, width = 5)
 
 #MARK - HOME SCREEN CODE
-greeting_for_home = tk.Label(home_frame, text="Welcome to home screen", height = 2)
+greeting_for_home = tk.Label(home_frame, text="Welcome to home screen", height = 2, font = title_font)
 home_screen_button_h = tk.Button(home_frame, text = "HOME", width = 20, height = 2, command = change_to_home_frame)
 test_screen_button_h = tk.Button(home_frame, text = "TEST", width = 20, height = 2, command = change_to_test_frame)
 learn_screen_button_h = tk.Button(home_frame, text = "LEARN", width = 20, height = 2, command = change_to_learn_frame)
@@ -49,9 +67,11 @@ home_screen_button_h.grid(column = 0, row = 1)
 test_screen_button_h.grid(column = 1, row = 1)
 learn_screen_button_h.grid(column = 2, row = 1)
 progress_screen_button_h.grid(column = 3, row = 1)
+separator = ttk.Separator(home_frame, orient = "horizontal")
+separator.grid(column = 0, row = 4, columnspan = 4, sticky = "ew", pady = 10)
 
 #MARK - TEST SCREEN CODE
-greeting_for_test = tk.Label(test_frame, text="TEST", height = 2)
+greeting_for_test = tk.Label(test_frame, text="TEST", height = 2, font = title_font)
 home_screen_button_t = tk.Button(test_frame, text = "HOME", width = 20, height = 2, command = change_to_home_frame)
 test_screen_button_t = tk.Button(test_frame, text = "TEST", width = 20, height = 2, command = change_to_test_frame)
 learn_screen_button_t = tk.Button(test_frame, text = "LEARN", width = 20, height = 2, command = change_to_learn_frame)
@@ -62,14 +82,47 @@ home_screen_button_t.grid(column = 0, row = 1)
 test_screen_button_t.grid(column = 1, row = 1)
 learn_screen_button_t.grid(column = 2, row = 1)
 progress_screen_button_t.grid(column = 3, row = 1)
+separator = ttk.Separator(test_frame, orient = "horizontal")
+separator.grid(column = 0, row = 4, columnspan = 4, sticky = "ew", pady = 10)
+topic_label = tk.Label(test_frame, text = "TOPIC NAME HERE" ,height = 1, font = topic_font)
+topic_label.grid(column = 0, row = 5, columnspan = 4)
+
+#CODE FOR WHEN ANSWER IS NOT SHOWN
+question_frame = tk.Frame(test_frame)
+question_label = tk.Label(question_frame,width = 75, height = 20, wraplength = 500, text = "qweuquefhysdfgysgyfugyuyyyugyuyuggyugyugyugyugyuuiawdhfuiashdfuiahsdfuihaisdufhasdfshadfjihasdjihasduifhhuiasdffhuiasdfihuasdfhuiasdf")
+question_label.grid(column = 0, row = 6, columnspan = 4)
+show_answer_button = tk.Button(question_frame, text = "Show Answer", command = showAnswer)
+show_answer_button.grid(column = 0, row = 7, columnspan = 4)
+question_frame.grid(column = 0, row = 6, columnspan = 4)
+
+
+    
+#CODE FOR WHEN ANSWER IS SHOWN
+answer_frame = tk.Frame(test_frame)
+
+green_button = tk.Button(answer_frame, bg = "green", text = "Very Confident", height = 5, width = 15)
+orange_button = tk.Button(answer_frame, bg = "orange", text = "Confident", height = 5, width = 15)
+yellow_button = tk.Button(answer_frame, bg = "yellow", text = "Mediocre", height = 5, width = 15)
+red_button = tk.Button(answer_frame, bg = "maroon", text = "Unconfident", height = 5, width = 15)
+green_button.grid(column = 0, row = 7)
+orange_button.grid(column = 1, row = 7)
+yellow_button.grid(column = 2, row = 7)
+red_button.grid(column = 3, row = 7)
+
+skip_button = tk.Button(answer_frame, text = "Skip")
+skip_button.grid(column = 0, row = 8, columnspan = 4, pady = 10)
+
+
 
 #MARK - LEARN SCREEN CODE
-greeting_for_learn = tk.Label(learn_frame, text="Welcome to learn screen", height = 2)
+greeting_for_learn = tk.Label(learn_frame, text="Welcome to learn screen", height = 2, font = title_font)
 home_screen_button_l = tk.Button(learn_frame, text = "HOME", width = 20, height = 2, command = change_to_home_frame)
 test_screen_button_l = tk.Button(learn_frame, text = "TEST", width = 20, height = 2, command = change_to_test_frame)
 learn_screen_button_l = tk.Button(learn_frame, text = "LEARN", width = 20, height = 2, command = change_to_learn_frame)
 progress_screen_button_l = tk.Button(learn_frame, text = "PROGRESS", width = 20, height = 2, command = change_to_progress_frame)
 
+separator = ttk.Separator(learn_frame, orient = "horizontal")
+separator.grid(column = 0, row = 4, columnspan = 4, sticky = "ew", pady = 10)
 greeting_for_learn.grid(column = 0, row = 0, columnspan = 4)
 home_screen_button_l.grid(column = 0, row = 1)
 test_screen_button_l.grid(column = 1, row = 1)
@@ -77,17 +130,54 @@ learn_screen_button_l.grid(column = 2, row = 1)
 progress_screen_button_l.grid(column = 3, row = 1)
 
 #MARK - PROGRESS SCREEN CODE
-greeting_for_progress = tk.Label(progress_frame, text="Welcome to progress screen", height = 2)
+greeting_for_progress = tk.Label(progress_frame, text="Welcome to progress screen", height = 2, font = title_font)
 home_screen_button_p = tk.Button(progress_frame, text = "HOME", width = 20, height = 2, command = change_to_home_frame)
 test_screen_button_p = tk.Button(progress_frame, text = "TEST", width = 20, height = 2, command = change_to_test_frame)
 learn_screen_button_p = tk.Button(progress_frame, text = "LEARN", width = 20, height = 2, command = change_to_learn_frame)
 progress_screen_button_p = tk.Button(progress_frame, text = "PROGRESS", width = 20, height = 2, command = change_to_progress_frame)
+separator = ttk.Separator(progress_frame, orient = "horizontal")
+separator.grid(column = 0, row = 4, columnspan = 4, sticky = "ew", pady = 10)
 
 greeting_for_progress.grid(column = 0, row = 0, columnspan = 4)
 home_screen_button_p.grid(column = 0, row = 1)
 test_screen_button_p.grid(column = 1, row = 1)
 learn_screen_button_p.grid(column = 2, row = 1)
 progress_screen_button_p.grid(column = 3, row = 1)
+
+def showProgress(subject):
+    #progress pane modification
+    progress_pane = Toplevel()
+    progress_pane.title(f"Progress for {subject}")
+    progress_pane.resizable(False, False)
+    pane_size_x = 400
+    pane_size_y = 300
+    progress_pane.geometry("{}x{}".format(pane_size_x, pane_size_y))
+
+    #adds the title of the progress tab
+    progress_font = tk.font.Font(family = "Helvetica", size = 16, weight = "bold")
+    progress_label = tk.Label(progress_pane, text = f"Progress for - {subject}", height =2, font = progress_font)
+    progress_label.grid(column = 0, row = 0, sticky = "ew", padx = 10)
+    
+    #add a divider below the progress icon
+    separator = ttk.Separator(progress_pane, orient = "horizontal")
+    separator.grid(column = 0, row = 1, columnspan = 4, sticky = "ew", padx = 10)
+
+    progress_dictionary = data_model.GetProgress(subject)
+    red_label = tk.Label(progress_pane, text = f"Red = {progress_dictionary['Red']}")
+    red_label.grid(column = 0, row = 2, sticky = "w", padx = 10)
+    orange_label = tk.Label(progress_pane, text = f"Orange = {progress_dictionary['Orange']}")
+    orange_label.grid(column = 0, row = 3, sticky = "w", padx = 10)
+    yellow_label = tk.Label(progress_pane, text = f"Yellow = {progress_dictionary['Yellow']}")
+    yellow_label.grid(column = 0, row = 4, sticky = "w", padx = 10)
+    green_label = tk.Label(progress_pane, text = f"Green = {progress_dictionary['Green']}")
+    green_label.grid(column = 0, row = 5, sticky = "w", padx = 10)
+    
+    Toplevel.mainloop(root)
+    
+test_label = tk.Label(progress_frame, text = "3 Data Representation")
+test_button = tk.Button(progress_frame, text = "Click here for progress", command = lambda: showProgress("3 Data Representation"))
+test_label.grid(column = 0, row = 6, columnspan = 4)
+test_button.grid(column = 0, row = 7, columnspan = 4)
 
 #main startup
 home_frame.pack(fill = "both") #pack the frames but grid the stuff within the frames
--- model.py
+++ model.py
@@ -1,105 +1,145 @@
 #data model - SQL database
 import mysql.connector
 
-def GetPassword():
-    password = ""
-    with open("password.txt") as file:
-        password = file.readline()
-    return password
+class Model():
 
-database = mysql.connector.connect(
-  user="root",
-  host = "localhost",
-  password=GetPassword(),
-  database="alca_db"
-)
-c = database.cursor(buffered=True)
+    def __init__(self):
+        self.database = mysql.connector.connect(
+          user="root",
+          host = "localhost",
+          password= self.GetPassword(),
+          database="alca_db"
+        )
+        self.c = self.database.cursor(buffered=True)
 
-print(database)
+    def GetPassword(self):
+        password = ""
+        with open("password.txt") as file:
+            password = file.readline()
+        return password
 
+    def GetTopicID(self, subject_name):
+        topicIDQuery = f"SELECT TopicID FROM topics WHERE TopicTitle = '{subject_name}'"
+        self.c.execute(topicIDQuery)
+        try:
+            topic_id = self.c.fetchall()[0][0]
+        except IndexError:
+            print("The topic name could not be found")
+        return topic_id
 
-def GetTableTitles():
-    c.execute("SHOW TABLES")
-    table_titles = []
-    for table in c:
-        table_titles.append(table[0])
-    return table_titles
+    def GetProgress(self, subject_name):
+        #selects the questions
+        topic_id = self.GetTopicID(subject_name)
 
-def PrintTables():
-    c.execute("SHOW TABLES")
-    print("TABLES ARE: ")
-    for table in c:
-        print(table)
+        #initially will select every question and then query from within that question
+        query = f"SELECT Confidence FROM questions WHERE TopicID = {topic_id}"
+        self.c.execute(query)
 
-def DeleteTable(table_name):
-    c.execute("DROP TABLE " + table_name)
+        red = 0
+        orange = 0
+        yellow = 0
+        green = 0
 
-def CreateTopic(topic_name, paper):
-    print("topic created.")
+        #returns a list of tuples containing ("red",) and nothing more
+        for result in self.c:
+            if result[0] == "red":
+                red += 1
+            elif result[0] == "orange":
+                orange += 1
+            elif result[0] == "yellow":
+                yellow += 1
+            elif result[0] == "green":
+                green +=1
 
-def ClearTable():
-    c.execute("DELETE FROM topics")
+        progress_dict = {
+            "Red": red,
+            "Orange": orange,
+            "Yellow": yellow,
+            "Green": green
+        }
 
-def InsertIntoTopics(topic_name, paper):
-    insert_statement = "INSERT INTO topics (TopicTitle, Paper) VALUES (%s, %s)"
-    values = (topic_name, paper)
-    c.execute(insert_statement, values)
-    database.commit()
+        return progress_dict
 
-def InsertIntoQuestions(topic_name, question_text, answer_type, answer_text):
-    topic_id = 1
-    topicIDQuery = f"SELECT TopicID FROM topics WHERE TopicTitle = '{topic_name}'"
-    c.execute(topicIDQuery)
-    topic_id = c.fetchall()[0][0]
-    print("topic id is " , topic_id)
-    insert_statement = "INSERT INTO questions (QuestionText, AnswerType, AnswerText, TopicID) VALUES (%s, %s, %s, %s)"
-    values = (question_text, answer_type, answer_text, topic_id)
-    c.execute(insert_statement, values)
-    database.commit()
+    def GetTableTitles(self):
+        self.c.execute("SHOW TABLES")
+        table_titles = []
+        for table in self.c:
+            table_titles.append(table[0])
+        return table_titles
 
-topics_table_creation = """
-CREATE TABLE topics (
-    TopicID INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    TopicTitle VARCHAR(255),
-    Paper INT
-);
-"""
-questions_table_creation = """
-CREATE TABLE questions (
-    QuestionID INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    QuestionText VARCHAR(255),
-    AnswerType VARCHAR(6),
-    AnswerText VARCHAR(1000),
-    Confidence VARCHAR(255) DEFAULT 'red',
-    TopicID INT,
-    FOREIGN KEY (TopicID) REFERENCES topics(TopicID)
-);
-"""
-#confidence could be either red orange green, or by default,
-def Main():
-    #start by creating the table for topics
-    PrintTables()
+    def PrintTables(self):
+        self.c.execute("SHOW TABLES")
+        print("TABLES ARE: ")
+        for table in self.c:
+            print(table)
 
-    #creation of the topics table
-    """
-    try:
-        c.execute(topics_table_creation)
-    except mysql.connector.Error as e:
-        if e.errno == 1050:
-            print("table already exists")
-        else :
-            print("other error occurred with error code : ", e.errno)
-    """
-
-    #creation of the questions table
-    try:
-        c.execute(questions_table_creation)
-    except mysql.connector.Error as e:
-        if e.errno == 1050:
-            print("table already exists")
-        else:
-            print("other error occured with error code : ", e.errno)
-            print("error message is: ", e.msg)
+    def DeleteTable(self,table_name):
+        self.c.execute("DROP TABLE " + table_name)
 
 
-Main()
+    def ClearTable(self):
+        self.c.execute("DELETE FROM topics")
+
+    def InsertIntoTopics(self, topic_name, paper):
+        insert_statement = "INSERT INTO topics (TopicTitle, Paper) VALUES (%s, %s)"
+        values = (topic_name, paper)
+        self.c.execute(insert_statement, values)
+        self.database.commit()
+
+    def InsertIntoQuestions(self, topic_name, question_text, answer_type, answer_text):
+        topic_id = self.GetTopicID(topic_name)
+
+        print("topic id is " , topic_id)
+        insert_statement = "INSERT INTO questions (QuestionText, AnswerType, AnswerText, TopicID) VALUES (%s, %s, %s, %s)"
+        values = (question_text, answer_type, answer_text, topic_id)
+        self.c.execute(insert_statement, values)
+        self.database.commit()
+
+
+    #confidence could be either red orange green, or by default,
+    def Main(self):
+
+        topics_table_creation = """
+        CREATE TABLE topics (
+            TopicID INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+            TopicTitle VARCHAR(255),
+            Paper INT
+        );
+        """
+        #CHANGING QUESTIONS TO TEST FOR A TEST, CHANGE BACK
+        questions_table_creation = """
+        CREATE TABLE test (
+            QuestionID INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+            QuestionText VARCHAR(255),
+            AnswerType VARCHAR(6),
+            AnswerText VARCHAR(1000),
+            Confidence VARCHAR(255) DEFAULT 'red',
+            TopicID INT,
+            FOREIGN KEY (TopicID) REFERENCES topics(TopicID)
+        );
+        """
+
+        #start by creating the table for topics
+        self.PrintTables()
+
+        #creation of the topics table
+        try:
+            self.c.execute(topics_table_creation)
+        except mysql.connector.Error as e:
+            if e.errno == 1050:
+                print("table already exists")
+            else :
+                print("other error occurred with error code : ", e.errno)
+
+
+        #creation of the questions table
+        try:
+            self.c.execute(questions_table_creation)
+        except mysql.connector.Error as e:
+            if e.errno == 1050:
+                print("table already exists")
+            else:
+                print("other error occured with error code : ", e.errno)
+                print("error message is: ", e.msg)
+
+model = Model()
